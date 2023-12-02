486236d42ad245802fa6d50d9f38faa820fd6152
Raul Ratiu <raulratiu2@gmail.com>
2023-12-03 00:02:42 +0200

    feat: add gRPC features

---
+++ gRPCClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gRPCClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
